447f2b85a9cee6d3882686ad5ef3eeea5616d887
Ted <tedhill07@icloud.com>
2024-09-07 15:17:16 +0100

    commist

--- Webscraping.py
+++ Webscraping.py
@@ -1,6 +1,14 @@
+#imports
 import requests
-import bs4 as bs
+from bs4 import BeautifulSoup
 import urllib.request
+import time as t
+#selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
 #Class init
 class movieStatsClass:
     def __init__(self, Title, Summary, Rating, ReleaseDate, Length,  Director, GenreList, posterLink):
@@ -13,7 +21,7 @@ class movieStatsClass:
         self.genreList = GenreList
         self.posterLink = posterLink
     def returnAsList(self):
-        return [self.title, self.summary, self.rating, self.releaseDate, self.length, self.director, self.genreList, self.posterLink]
+        return [self.title, self.summary, self.rating, self.releaseDate, self.length, self.director, self.genreList, self.posterLink] 
 
 #The Moviedb
 global genreDict
@@ -30,7 +38,7 @@ def returnMovieDBData(movieName = "none", Moviedb_APIKEY = "66ab025a7673a17b6e97
     if movieName == "none":
         return ["Movie name missing/invalid"]
     #Getting General Data
-    dataURL = f"https://api.themoviedb.org/3/search/movie?api_key={Moviedb_APIKEY}&query={movieName}&append_to_response=runtime"    
+    dataURL = f"https://api.themoviedb.org/3/search/movie?api_key={Moviedb_APIKEY}&query={movieName}&append_to_response=runtime"
     response = requests.get(dataURL)
     data = response.json()
     #NEED TO WORK ON
@@ -50,9 +58,9 @@ def returnMovieDBData(movieName = "none", Moviedb_APIKEY = "66ab025a7673a17b6e97
     genreList = []
     for genre in genreIDList:
         genreList.append(genre["name"])
-    #checking if runtime returns 'None'
+    #checking if runtime returns 'None' as it keeps returning none
     if not movie.get("runtime"):
-        print(f"Error returning '{movieName.title()}'runtime, defaulting to 138.")
+        print(f"Error returning '{movieName.title()}' runtime, defaulting to 138.")
         movieRuntime = 138
     else:
         movieRuntime = movie.get("runtime")
@@ -61,23 +69,43 @@ def returnMovieDBData(movieName = "none", Moviedb_APIKEY = "66ab025a7673a17b6e97
             movie["overview"],
             movie["vote_average"],
             movie["release_date"],
-            movieRuntime, #keeps returning "None"
+            movieRuntime,
             directorList[0],
             genreList,
             f"https://image.tmdb.org/t/p/original/{movie["poster_path"]}"]
-#Odeon - bs4 not work
-    #bs4 doesn't work
-pass
+#Odeon
+print("ODEON")
+print("init firefox driver")
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+driver = webdriver.Firefox()
+
+print("open website page")
+
+driver.get("https://www.odeon.co.uk")
+
+print("Sleep")
+#sleep for website to load
+t.sleep(5)
+
+print("Sleep Done.\n Press Accept")
+#press the accept button for ODEN website
+driver.find_element(By.ID, "onetrust-accept-btn-handler").click()
+
+print("Look for search bar.")
+t.sleep(1)
+#click on search button
+driver.find_element(By.CLASS_NAME, "banner-icon").click()
+print("typing in search bar.")
+t.sleep(0.5)
+#find search page
+searchBar = driver.find_element(By.CLASS_NAME, "auto-complete")
+driver.quit()
+
 #Showcase
     #bs4 works
-showcaseSourceURL = urllib.request.urlopen("").read()
-soup = bs.BeautifulSoup(showcaseSourceURL, "lxml")
-
-pass
+"""showcaseSourceURL = urllib.request.urlopen("https://www.showcasecinemas.co.uk/movies/251633-deadpool-and-wolverine/").read()
+soup = BeautifulSoup(showcaseSourceURL, "lxml")"""
 #Vue
-
-pass
 #Cineworld
-pass
 #Savoy
-pass
