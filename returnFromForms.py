b70e3ec0b6759fe0016f7cd84b2e642ed5e6b27d
Ted <tedhill07@icloud.com>
2024-11-03 15:59:06 +0000

    dev POST signup

--- returnFromForms.py
+++ returnFromForms.py
@@ -2,17 +2,23 @@
 import urllib.request, json #not me
 from flask import request #not me
 from werkzeug.datastructures import ImmutableMultiDict  #not me
+import Registration as reg
 #functions
-def returnSignFormData(pageData = None):
-    pass
 
-ImmutableMultiDict([('fname', 'ted'), 
+def returnSignFormData(pageData = None): #returns class
+    dic = {key : pageData.getlist(key)[0] for key in pageData} #painful to read
+    if dic["password1"] == dic["password2"]:
+        passwordCheck = reg.checkPassword(dic)
+        userClassInstance = reg.userClass()
+
+trial = ImmutableMultiDict([('fname', 'ted'), 
                     ('lname', 'hill'), 
                     ('uname', 'doggo'), 
                     ('gender', 'M'), 
                     ('email', 'tedhill07@icloud.com'), 
-                    ('password1', 'EdwardHill12'), 
-                    ('password2', 'EdwardHill12'), 
+                    ('password1', 'DogRizz123'), 
+                    ('password2', 'DogRizz123'), 
                     ('submit', 'Create Account')])
 
-print(ImmutableMultiDict.getlist("fname"))
+dic = returnSignFormData(trial)
+print(dic)
