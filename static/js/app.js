edd1f658716206943ea3d40e6b274fa620debef1
Ted <tedhill07@icloud.com>
2024-12-30 00:18:35 +0000

    check email JS

--- static/js/app.js
+++ static/js/app.js
@@ -15,6 +15,11 @@ function checkPassword(password) {
     return true;
 }
 
+function checkEmail(email) {
+    let emailString = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return emailString.test(email);
+}
+
 // Test //
 var testPassword = "dog";
 console.log(hasNumber(testPassword));
